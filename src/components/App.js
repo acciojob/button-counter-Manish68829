2380075a6f9869d93f7cf91c5df4c82a98e1e71f
Manish <121147194+Manish68829@users.noreply.github.com>
2023-06-26 13:35:58 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+
 
 const App = () => {
+
+  const [Counter, setCounter]=useState(0);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {Counter} times</p>
+        <button onClick={()=>setCounter(Counter+1)}>Click me</button>
+        
     </div>
   )
 }
